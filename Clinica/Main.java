2e99a7141a50db9c17a4de09b5223bccd6243b6b
millenaunicap <142348699+millenaunicap@users.noreply.github.com>
2024-03-20 10:36:58 -0300

    Update Main.java

--- Clinica/Main.java
+++ Clinica/Main.java
@@ -2,4 +2,19 @@ package Clinica;
 
 public class Main {
 
+     public static void main(String[] args) {
+	       
+	        Endereco endereco = new Endereco("Rua A", 123, "Apto 101", "Centro", "Cidade", "UF", "12345-678");
+
+	       
+	        System.out.println("Endereço:");
+	        System.out.println("Rua: " + endereco.getRua());
+	        System.out.println("Número: " + endereco.getNumero());
+	        System.out.println("Complemento: " + endereco.getComplemento());
+	        System.out.println("Bairro: " + endereco.getBairro());
+	        System.out.println("Cidade: " + endereco.getCidade());
+	        System.out.println("UF: " + endereco.getUf());
+	        System.out.println("CEP: " + endereco.getCep());
+	    }
+
 }
